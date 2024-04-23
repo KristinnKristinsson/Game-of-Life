fcec90a1cc8577925922f5c6d7a2483c10266791
KristinnKristinsson <krkr2000@student.miun.se>
2024-04-23 12:34:56 +0200

    A 10x10 screen created
    A window has appeared. Static placement. No cursor.

--- main.c
+++ main.c
@@ -3,9 +3,37 @@
 #include <math.h>
 #include <string.h>
 
+#define ROWS 10
+#define COLUMNS 10
+
 int main()
 {
-    
-    /* code */
+    int quit = 0;
+    while (!quit)
+    {
+        //Hide cursor
+        printf("\e[?25l");
+
+        printf("8");
+        for (int i = 0; i < COLUMNS; i++)
+            printf("-");
+        printf("8\n");
+
+        for (int j = 0; j < ROWS; j++)
+        {
+            printf("|");
+            for (int i = 0; i < COLUMNS; i++)
+                printf(".");
+            printf("|\n");
+        }
+
+        printf("8");
+        for (int i = 0; i < COLUMNS; i++)
+            printf("-");
+        printf("8\n");
+
+        //move cursor to top
+        printf("\e[%iA", ROWS + 2);
+    }
     return 0;
 }
